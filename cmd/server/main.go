5376aed4f405f2901bc17d85a8f0c6ed18addca8
iAmHuyyy <ngnhathuy1305@gmail.com>
2026-04-11 14:17:13 +0700

    create checkout

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -30,11 +30,13 @@ func main() {
 
 	authBiz := business.NewAuthBusiness(userRepo, affiliationRepo, cfg)
 	adminBiz := business.NewAdminBusiness(userRepo)
+	affiliationBiz := business.NewAffiliationBusiness(affiliationRepo)
 
 	authHandler := handler.NewAuthHandler(authBiz)
 	adminHandler := handler.NewAdminHandler(adminBiz)
+	affiliationHandler := handler.NewAffiliationHandler(affiliationBiz)
 
-	r := router.Setup(authHandler, adminHandler, userRepo, cfg)
+	r := router.Setup(authHandler, adminHandler, affiliationHandler, cfg, userRepo)
 	log.Printf("Server starting on :%s", cfg.ServerPort)
 	if err := r.Run(":" + cfg.ServerPort); err != nil {
 		log.Fatal(err)
