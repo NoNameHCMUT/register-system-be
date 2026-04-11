1f8b8f39796251aa6699eeaac64e429a12238c4e
tinletrongCS <lttin605@gmail.com>
2026-04-11 15:37:50 +0700

    add view projects

--- model/response.go
+++ model/response.go
@@ -49,3 +49,51 @@ func ToUserResponse(u *User) UserResponse {
 	}
 	return resp
 }
+
+type ProjectResponse struct {
+	ID              uint                 `json:"id"`
+	Name            string               `json:"name"`
+	Description     string               `json:"description"`
+	NumMax          uint                 `json:"num_max"`
+	NumAttending    uint                 `json:"num_attending"`
+	ProjectStartDay string               `json:"project_start_day"`
+	ProjectEndDay   string               `json:"project_end_day"`
+	FormStartDay    string               `json:"form_start_day"`
+	FormEndDay      string               `json:"form_end_day"`
+	AffiliationID   uint                 `json:"affiliation_id"`
+	Affiliation     *AffiliationResponse `json:"affiliation,omitempty"`
+	CommunityUser   *UserResponse        `json:"community_user,omitempty"`
+	CreatedAt       string               `json:"created_at"`
+}
+
+func ToProjectResponse(p *Project) ProjectResponse {
+	resp := ProjectResponse{
+		ID:              p.ID,
+		Name:            p.Name,
+		Description:     p.Description,
+		NumMax:          p.NumMax,
+		NumAttending:    p.NumAttending,
+		ProjectStartDay: p.ProjectStartDay.Format("2006-01-02T15:04:05Z07:00"),
+		ProjectEndDay:   p.ProjectEndDay.Format("2006-01-02T15:04:05Z07:00"),
+		FormStartDay:    p.FormStartDay.Format("2006-01-02T15:04:05Z07:00"),
+		FormEndDay:      p.FormEndDay.Format("2006-01-02T15:04:05Z07:00"),
+		AffiliationID:   p.AffiliationID,
+		CreatedAt:       p.CreatedAt.Format("2006-01-02T15:04:05Z07:00"),
+	}
+	if p.Affiliation.ID != 0 {
+		resp.Affiliation = &AffiliationResponse{
+			ID:      p.Affiliation.ID,
+			StdName: p.Affiliation.StdName,
+		}
+	}
+	if p.CommunityUser.ID != 0 {
+		resp.CommunityUser = &UserResponse{
+			ID:            p.CommunityUser.ID,
+			Username:      p.CommunityUser.Username,
+			FullName:      p.CommunityUser.FullName,
+			Email:         p.CommunityUser.Email,
+			Role:          p.CommunityUser.Role,
+		}
+	}
+	return resp
+}
