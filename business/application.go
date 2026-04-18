dfd5d4c89cd3e369c91016ec86802a9c9afa71e0
bibimoni <proquadi66@gmail.com>
2026-04-18 14:25:22 +0700

    chore(application): remove unused fmt import and debug print statement

--- business/application.go
+++ business/application.go
@@ -3,7 +3,6 @@ package business
 import (
 	"errors"
 	"time"
-	"fmt"
 	"register-system-be/email"
 	"register-system-be/model"
 	"register-system-be/repo"
@@ -220,7 +219,6 @@ func (b *applicationBusiness) SchoolAction(schoolUserID uint, req *model.Applica
 		if project.AffiliationID != user.AffiliationID {
 			continue
 		}
-		fmt.Printf("Test $v", err)
 		if err := b.applicationRepo.UpdateStatus(id, targetStatus); err != nil {
 			continue
 		}
